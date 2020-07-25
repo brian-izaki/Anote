f1b306388a108f59b76eb2176537b8c0a170a831
Brian_Izaki <bkizaki@outlook.com>
2020-07-25 16:51:13 -0300

    add imagem no ButtomAdd

--- src/components/ButtonAdd/index.js
+++ src/components/ButtonAdd/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import './style.css';
+// import imgAdd from '../../imagens/icone_mais.png';
+
 
 export default function ButtonAdd(props){
   return(
+    // style={{backgroundImage: `URL(${imgAdd})`}}
     <div className="btn-add">
 
     </div>
