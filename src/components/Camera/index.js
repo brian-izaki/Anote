0c4df0891f3e50e404da6934eaed245e339ffe19
Brian_Izaki <bkizaki@outlook.com>
2020-08-06 12:21:38 -0300

    add funcionalidade de pausar video

--- src/components/Camera/index.js
+++ src/components/Camera/index.js
@@ -8,6 +8,7 @@ import "./style.css";
 
 export default function Camera() {
   const [video, setVideo] = useState({});
+  const [estado, setEstado] = useState({ videoOn: true });
   const canvas = document.querySelector("canvas");
 
   /**
@@ -15,11 +16,15 @@ export default function Camera() {
    * o componente renderiza duas vezes
    * (provavelmente por causa do setState dentro do useEffect)
    */
-  useEffect(() => {
-    // useEffect renderiza antes de todo o componente
-    // por isso precisa que estas variaveis fiquem dentro dele
-    setVideo(document.querySelector("#camera"));
+  ////useEffect(() => {
+  // useEffect renderiza antes de todo o componente
+  // por isso precisa que estas variaveis fiquem dentro dele
 
+  useEffect(() => {
+    setVideo(document.querySelector("#camera"));
+  }, []);
+
+  function ativaDesativa() {
     // funcionalidades do video
     const constraints = {
       audio: false,
@@ -28,7 +33,6 @@ export default function Camera() {
 
     function handleSucces(stream) {
       console.log("sucesso");
-
       window.stream = stream;
       video.srcObject = stream;
     }
@@ -37,32 +41,39 @@ export default function Camera() {
       console.log("Erro: ", error.message);
     }
 
-    // permissão de acesso e constraints do que será acessado
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then(handleSucces)
-      .catch(handleErrors);
-      
-  }, [video.srcObject]);
+    function stopVideo(stream) {
+      const videoTracks = stream.getVideoTracks();
+      const mediaAtual = videoTracks[0];
+
+      mediaAtual.stop();
+    }
+
+    if (estado.videoOn) {
+      // permissão de acesso e constraints do que será acessado
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(handleSucces)
+        .catch(handleErrors);
+    } else {
+      stopVideo(video.srcObject);
+    }
+  }
+  ////}, [estado.videoOn, video, video.srcObject]);
 
   function createPicture(e) {
     e.preventDefault();
-
     canvas.width = video.clientWidth;
     canvas.height = video.clientHeight;
-
-    console.log("teste camera video:", video.width);
     canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
   }
 
   function clearPicture(e) {
     e.preventDefault();
-
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
   }
 
   return (
-    <div className="">
+    <div>
       <p>
         <b>Anotação:</b>
       </p>
@@ -74,6 +85,15 @@ export default function Camera() {
         <div>
           <ButtonCamera onClick={clearPicture} img={imgSemCamera} />
           <ButtonCamera onClick={createPicture} img={imgCamera} />
+          <ButtonCamera
+            onClick={(e) => {
+              e.preventDefault();
+              clearPicture(e);
+              setEstado({ videoOn: !estado.videoOn });
+              ativaDesativa();
+            }}
+            img={imgCamera}
+          />
         </div>
       </div>
     </div>
