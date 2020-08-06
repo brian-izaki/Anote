446b7a00d13d74ee3a6789ae2d8553f7ec2478f0
Brian_Izaki <bkizaki@outlook.com>
2020-08-06 00:46:24 -0300

    foto tirada aparece canvas e excluida volta o video

--- src/components/Camera/index.js
+++ src/components/Camera/index.js
@@ -1,74 +1,79 @@
-import React, { useEffect, useState } from 'react';
-import ButtonCamera from './components/ButtonCamera';
+import React, { useEffect, useState } from "react";
+import ButtonCamera from "./components/ButtonCamera";
 
-import imgCamera from '../../imagens/icone_camera.png';
-import imgSemCamera from '../../imagens/icone_sem_camera.png';
+import imgCamera from "../../imagens/icone_camera.png";
+import imgSemCamera from "../../imagens/icone_sem_camera.png";
 
-import './style.css';
+import "./style.css";
 
-export default function Camera(){
-  
+export default function Camera() {
   const [video, setVideo] = useState({});
-  const canvas = document.querySelector('canvas');
-  
+  const canvas = document.querySelector("canvas");
+
   /**
    * Necessário refatorar
-   * o componente renderiza duas vezes 
-   * (provavelmente por causa do setState dentro do useEffect) 
+   * o componente renderiza duas vezes
+   * (provavelmente por causa do setState dentro do useEffect)
    */
   useEffect(() => {
     // useEffect renderiza antes de todo o componente
-    // por isso precisa que estas variaveis fiquem dentro dele 
-    setVideo(document.querySelector('#camera'));
-    
+    // por isso precisa que estas variaveis fiquem dentro dele
+    setVideo(document.querySelector("#camera"));
+
     // funcionalidades do video
     const constraints = {
       audio: false,
       video: true,
-    }
-    
-    function handleSucces(stream){
-      console.log('sucesso')
-      
+    };
+
+    function handleSucces(stream) {
+      console.log("sucesso");
+
       window.stream = stream;
       video.srcObject = stream;
     }
-    
-    function handleErrors(error){
-      console.log('Erro: ', error.message)
+
+    function handleErrors(error) {
+      console.log("Erro: ", error.message);
     }
-    
+
     // permissão de acesso e constraints do que será acessado
-    navigator.mediaDevices.getUserMedia(constraints).then(handleSucces).catch(handleErrors)
-    
-  }, [video.srcObject])
-  
-  function createPicture(e){
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(handleSucces)
+      .catch(handleErrors);
+      
+  }, [video.srcObject]);
+
+  function createPicture(e) {
     e.preventDefault();
 
     canvas.width = video.clientWidth;
     canvas.height = video.clientHeight;
-    
-    console.log('teste camera video:', video.width)
-    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    console.log("teste camera video:", video.width);
+    canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
   }
-  
-  function clearPicture(e){
+
+  function clearPicture(e) {
     e.preventDefault();
 
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
   }
 
   return (
-    <div className="camera-container">
-      <p><b>Anotação:</b></p>
-      <video id="camera" playsInline autoPlay></video>
-      <canvas></canvas>
+    <div className="">
+      <p>
+        <b>Anotação:</b>
+      </p>
+      <div className="camera-container">
+        <video id="camera" playsInline autoPlay></video>
+        <canvas></canvas>
+      </div>
       <div>
         <div>
-
           <ButtonCamera onClick={clearPicture} img={imgSemCamera} />
-          <ButtonCamera onClick={createPicture} img={imgCamera}/>
+          <ButtonCamera onClick={createPicture} img={imgCamera} />
         </div>
       </div>
     </div>
