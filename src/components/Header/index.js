60b2676f37fda0baa279649d768155d21a4a1a10
Brian_Izaki <bkizaki@outlook.com>
2020-08-08 17:56:20 -0300

    Add botao de voltar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import './style.css';
+import imgArrowLeft from '../../imagens/icone_esquerda.png';
+import { Link } from "react-router-dom";
 
-export default function Header(props) {
+export default function Header({principal, children}) {
   return (
     <div className="header">
-      <h1>{props.titulo}</h1>
+      <div>
+        {
+          !principal
+            ?(<Link className="link" to="/">
+                <img src={imgArrowLeft} alt="Voltar"/>
+              </Link>)
+            : null
+        }
+        <h1 className="titulo">{children}</h1>
+      </div>
     </div>
   );
 }
